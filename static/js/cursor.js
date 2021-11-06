9bebb02ff70180c3040fa625ffe73bd2c8919cb4
yayab42 <yannis.bounia@outlook.com>
2021-11-06 20:21:32 +0100

    Toggle burger menu - no animation

--- static/js/cursor.js
+++ static/js/cursor.js
@@ -12,10 +12,10 @@ window.addEventListener('mousemove', move);
 
 function move(e){
   for (var ii = 1; ii < numP; ii++) {
-    TweenMax.to('#p'+ii, .5,{delay:.005*ii, bezier:{type:'soft', values:[{x:Math.max(Math.min(e.clientX+25-50*Math.random(), window.innerWidth-21), 21),
-                                                                          y:Math.max(Math.min(e.clientY+25-50*Math.random(), window.innerHeight-21), 21)}, 
-                                                                         {x:Math.max(Math.min(e.clientX, window.innerWidth-21), 21), 
-                                                                          y:Math.max(Math.min(e.clientY,window.innerHeight-21),21)}]}, 
+    TweenMax.to('#p'+ii, .5,{delay:.005*ii, bezier:{type:'soft', values:[{x:Math.max(Math.min(e.clientX+25-50*Math.random(), window.innerWidth-2), 2),
+                                                                          y:Math.max(Math.min(e.clientY+25-50*Math.random(), window.innerHeight-2), 2)}, 
+                                                                         {x:Math.max(Math.min(e.clientX, window.innerWidth-2), 2), 
+                                                                          y:Math.max(Math.min(e.clientY,window.innerHeight-2),2)}]}, 
                                                                           ease:Back.easeOut.config(3)})
     TweenMax.to('#p'+ii, .005*ii,{scale:4})
     TweenMax.to('#p'+ii, .5,{delay:.005*ii, scale:0})    
